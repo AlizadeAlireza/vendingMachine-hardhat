082cb3d172daee579294c55e05a71c71d95cb5eb
alireza alizadeh <alireza.aa9700@gmail.com>
2023-04-27 11:47:40 +0330

    getter functions place is changed

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -2,8 +2,18 @@
 
 pragma solidity ^0.8.11;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/*  custom errors  */
+
+error VendingMachine__();
+error VendingMachine__1();
+error VendingMachine__12();
+
 contract VendingMachine {
+    /*  variables  */
     address public owner;
+
     mapping(address => uint256) public donutBalances;
 
     constructor() {
@@ -13,30 +23,17 @@ contract VendingMachine {
 
     // anyone should be able to set amount of donut
     // getting total balance
-    function getVendingMachineBalance() public view returns (uint256) {
-        return donutBalances[address(this)];
-    }
-
-    function getBuyerBalancer() public view returns (uint256) {
-        return donutBalances[msg.sender];
-    }
 
     // because update the value --> don't use the view or pure
     function restock(uint256 _amount) public {
-        require(
-            msg.sender == owner,
-            "only the owner can restock this machine."
-        );
+        require(msg.sender == owner, "only the owner can restock this machine.");
         donutBalances[address(this)] += _amount;
     }
 
     // payable for receive ether
     function purchase(uint256 _amount) public payable {
         // we need to check purchaser send enough money
-        require(
-            msg.value >= _amount * 2 ether,
-            "You must pay at least 2 Ether per donut"
-        );
+        require(msg.value >= _amount * 2 ether, "You must pay at least 2 Ether per donut");
         // enough donuts in the vending machine for requests
         require(
             donutBalances[address(this)] >= _amount,
@@ -45,5 +42,15 @@ contract VendingMachine {
 
         donutBalances[address(this)] -= _amount;
         donutBalances[msg.sender] += _amount;
+    }
+
+    /*  getter functions  */
+
+    function getVendingMachineBalance() public view returns (uint256) {
+        return donutBalances[address(this)];
+    }
+
+    function getBuyerBalancer() public view returns (uint256) {
+        return donutBalances[msg.sender];
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -71,6 +71,9 @@ module.exports = {
     solidity: {
         compilers: [
             {
+                version: "0.8.11",
+            },
+            {
                 version: "0.8.9",
             },
             {
